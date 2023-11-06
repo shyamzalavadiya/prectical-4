0a27f60d2d5a531a6c20c1ede69574c459bc5901
shyamzalavadiya <141892129+shyamzalavadiya@users.noreply.github.com>
2023-11-06 21:44:35 +0530

    p-4-10.cpp

---
+++ p-4-10.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class get{
+	private:
+		int number1,number2,result;
+	public:
+		get(){
+		cout<<"Enter your first number :";
+		cin>>number1;
+		cout<<"Enter your second number :";
+		cin>>number2;
+		result=number1+number2;
+		cout<<"sum of given number is ::"<<result;
+		}		
+};
+int main()
+{
+	get g;
+	cout <<"shyam zalavadiya";
+	cout<<"2201301815";
+	return 0;
+}
+
